421d789527fff29b9fa2b6a880b0243746f0e97e
spacesamurai <spacesamurai@epicloot.us>
2020-03-02 03:20:13 -0600

    Newest Bracnch

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,9 +1,25 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-splashscreen-greetgo/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen-greetgo.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen-greetgo",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen-greetgo/src/browser/SplashScreenProxy.js",
+        "id": "cordova-plugin-splashscreen-greetgo.SplashScreenProxy",
+        "pluginId": "cordova-plugin-splashscreen-greetgo",
+        "runs": true
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.3"
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-plugin-splashscreen-greetgo": "1.0.2"
 }
 // BOTTOM OF METADATA
 });
--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,6 +1,16 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
+  module.exports = [
+    {
+      "id": "cordova-plugin-splashscreen-greetgo.SplashScreen",
+      "file": "plugins/cordova-plugin-splashscreen-greetgo/www/splashscreen.js",
+      "pluginId": "cordova-plugin-splashscreen-greetgo",
+      "clobbers": [
+        "navigator.splashscreen"
+      ]
+    }
+  ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.3"
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-plugin-splashscreen-greetgo": "1.0.2"
   };
 });
